c9af4e7af4adfe5499aace290f6bce0c963c2a1d
Bob Reselman <bob@CogArtTech.com>
2023-06-10 17:47:05 -0700

    refactoring project

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,38 +1,3 @@
-import {Gender} from './enums'
-
-export function scream(prefix: string): Function {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        const originalMethod = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-            const result = originalMethod.apply(this, args);
-            return `${prefix} ${result.toUpperCase()}`;
-        };
-    };
-}
-
-export const asDays = (target: any, memberName: string, descriptor: PropertyDescriptor) => {
-    const originalMethod = descriptor.value;
-    descriptor.value = function (...args: any[]) {
-        const result = originalMethod.apply(this, args);
-        return result * 365;
-    };
-}
-
-export const asAbbreviation = (target: any, memberName: string, descriptor: PropertyDescriptor) => {
-    const originalMethod = descriptor.value;
-    descriptor.value = function (...args: any[]) {
-        const result = originalMethod.apply(this, args);
-        switch (result) {
-            case Gender.MALE:
-                return 'M';
-            case Gender.FEMALE:
-                return 'F';
-            case Gender.NON_BINARY:
-                return 'N';
-        }
-    };
-}
-
 /***************************************
  * This is a method decorator that adds before
  * and after logging behavior to a decorated method
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import {Person} from './person'
-import {Gender} from './enums'
-import {Birthdate} from "./birthdate";
-const dob = new Birthdate(10,1,1,);
-const person = new Person('Bob', 'Reselman', dob, Gender.MALE);
+import {Printer} from "./printer";
 
-console.log(person.getFullName())
-
-console.log(person.getAge())
-
-console.log(person.getGender())
+const printer = new Printer();
+printer.print('Decorators are cool!');
